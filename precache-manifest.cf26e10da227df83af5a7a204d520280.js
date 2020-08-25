e254adfd4cdf3040b92dc153d6ddf942cbf09b4b
Sithi Nasila <Sithi.nasila@gmail.com>
2020-08-25 16:37:16 +0530

    Updates

---
+++ precache-manifest.cf26e10da227df83af5a7a204d520280.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f369537d062d94a3db59fc6cd3913145",
+    "url": "/github-issues-clone/index.html"
+  },
+  {
+    "revision": "76d1676387fac1e4f641",
+    "url": "/github-issues-clone/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "7e6996020d06b3596406",
+    "url": "/github-issues-clone/static/css/main.5277d419.chunk.css"
+  },
+  {
+    "revision": "76d1676387fac1e4f641",
+    "url": "/github-issues-clone/static/js/2.8b47a7cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/github-issues-clone/static/js/2.8b47a7cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e6996020d06b3596406",
+    "url": "/github-issues-clone/static/js/main.b3d8c2f1.chunk.js"
+  },
+  {
+    "revision": "cd4196b1671a3d04f345",
+    "url": "/github-issues-clone/static/js/runtime-main.f0774f8c.js"
+  }
+]);
